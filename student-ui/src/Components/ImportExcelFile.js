e60c7fb3366c6970ddae9f311401d36979a95e9f
MOHANRAJ, SRAVAN KUMAR <sravan.k.mohanraj@accenture.com>
2021-10-17 19:35:37 +0530

    Initial UI had done

---
+++ student-ui/src/Components/ImportExcelFile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ImportExcelFile = () => {
+    return (
+        <div>
+            <form>
+                
+            </form>
+        </div>
+    )
+}
+
+export default ImportExcelFile
